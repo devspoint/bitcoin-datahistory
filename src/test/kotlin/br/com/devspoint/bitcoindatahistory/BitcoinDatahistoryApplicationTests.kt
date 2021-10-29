e6761b925172d98cdc7bdb2a1dfe1e76655a7077
Felipe Formentin <felipe.formentin@mercadolivre.com>
2021-10-29 14:04:59 -0300

    Initial commit

---
+++ src/test/kotlin/br/com/devspoint/bitcoindatahistory/BitcoinDatahistoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.devspoint.bitcoindatahistory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BitcoinDatahistoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
